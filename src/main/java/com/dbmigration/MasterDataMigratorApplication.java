dc4bf6e3280a22d6e8d32adfcd96a8661755a396
Anubhav <anubhav1121singh@gmail.com>
2026-07-30 12:24:02 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/dbmigration/MasterDataMigratorApplication.java
@@ -0,0 +1,13 @@
+package com.dbmigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterDataMigratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterDataMigratorApplication.class, args);
+	}
+
+}
